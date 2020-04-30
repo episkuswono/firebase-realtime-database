8a9a9c5a098a47da8e3d8f1cfba6c38774eca45e
episkuswono <episkuswono@gmail.com>
2020-04-30 09:27:05 +0700

    create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCXRnVg4YsnftCEn4I18hwcA4F9aXhABLE",
+  authDomain: "reactjs-39b2f.firebaseapp.com",
+  databaseURL: "https://reactjs-39b2f.firebaseio.com",
+  projectId: "reactjs-39b2f",
+  storageBucket: "reactjs-39b2f.appspot.com",
+  messagingSenderId: "636857133268",
+  appId: "1:636857133268:web:8e5b46b8c7ad19cdd37341",
+  measurementId: "G-HLKK6Q2KTH"
+};
+
+export default firebaseConfig;
